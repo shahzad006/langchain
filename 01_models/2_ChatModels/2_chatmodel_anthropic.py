6c882903eb40e492bb0ab9ac0cd5ffcb0e2b65b3
Muhammad Shahzad <muhammadshahzad45435@gmail.com>
2026-09-10 12:05:44 +0500

    ChatModels Anthropic

---
+++ 01_models/2_ChatModels/2_chatmodel_anthropic.py
@@ -0,0 +1,13 @@
+from langchain_anthropic import ChatAnthropic
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+
+model = ChatAnthropic(model="claude-3-5-sonnet-20241022")
+
+
+result = model.invoke("What is the capital of Pakistan")
+
+print(result.content)
