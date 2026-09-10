4924b3d61623731cfa59b98a7ac1a1b34fdfcbe0
Muhammad Shahzad <muhammadshahzad45435@gmail.com>
2026-09-10 11:42:17 +0500

    ChatModels Openai

---
+++ 01_models/2_ChatModels/1_chatmodel_openai.py
@@ -0,0 +1,14 @@
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+
+model = ChatOpenAI(model="gpt-4")
+
+
+result = model.invoke("What is the capital of Pakistan?")
+
+
+print(result.content)
