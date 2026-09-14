16b068fb696c5841c4ab8f71ca2fb0478db0b9e9
Muhammad Shahzad <muhammadshahzad45435@gmail.com>
2026-09-14 22:10:23 +0500

    ChatModel Google

---
+++ 01_models/2_ChatModels/3_chatmodel_google.py
@@ -0,0 +1,17 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+
+
+model = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
+
+result = model.invoke("What is the Capital of Pakistan")
+
+
+print(result.content)
+
+
+
